a8037302abba4c8d28e9d259b5137d8dc018b006
George Ashworth <georgeliuashworth@gmail.com>
2014-02-11 23:04:37 -0500

    wired posts collection to postsList template

--- microscope/client/views/posts/posts_list.js
+++ microscope/client/views/posts/posts_list.js
@@ -1,21 +1,5 @@
-var postsData = [
-	{
-		title: 'Developing Web Applications with Meteor',
-		author: 'George Ashworth',
-		url: 'http://georgeashworth.com'
-	},
-	{
-		title: 'Meteor',
-		author: 'Tom Coleman',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
-	}
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() { 
+		return Posts.find();
+	}
 });
