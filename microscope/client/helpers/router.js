22832815cce31e1b179d887a4fd726bde3ec81b1
George Ashworth <georgeliuashworth@gmail.com>
2014-02-13 19:13:45 -0500

    added submit post page and linked to it in the header

--- microscope/client/helpers/router.js
+++ microscope/client/helpers/router.js
@@ -1,8 +1,16 @@
+Router.configure({
+	layoutTemplate: 'layout'
+});
+
 Router.map(function() {
 	this.route('postsList', {path: '/'});
 
 	this.route('postPage', {
 		path: '/posts/:_id',
 		data: function() { return Posts.findOne(this.params._id); }
+	});	
+
+	this.route('postSubmit', {
+		path: '/submit'	
 	});
 });
